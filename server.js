0faf50962f654d82d15e132929454088b111f8ef
JalissaRandle <Jalissar18@Gmail.com>
2022-04-07 13:13:25 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '1c5c3bc5f0e9422e918dabc13ccade6e',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 4040
+
+app.listen(port, () => console.log(`Server is listening on ${port}`))
