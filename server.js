ac2f3fb40f4dd3ef3b17e60c29cd8ef00d6efd2b
JalissaRandle <Jalissar18@Gmail.com>
2022-04-07 14:36:44 -0500

    update 4

--- server.js
+++ server.js
@@ -16,32 +16,47 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
 
-const pets = ['dogs', 'cats', 'fish']
+const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get('/api/pets/', (req, res) => {
-    res.status(200).send(pets)
-    rollbar.log('someone requested to see the pets')
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+    rollbar.log('someone requested the student list.')
 })
 
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
 
+   const index = students.findIndex(student => {
+       return student === name
+   })
 
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+           res.status(400).send('You must enter a name.')
+       } else {
+           res.status(400).send('That student already exists.')
+       }
+       rollbar.info('someone added a student')
+   } catch (err) {
+       console.log(err)
+       rollbar.error(`${err} triggered in the post request to /api/students `) 
+   }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+    rollbar.log('someone deleted student')
+})
 const port = process.env.PORT || 4040
 
 app.listen(port, () => console.log(`Server is listening on ${port}`))
